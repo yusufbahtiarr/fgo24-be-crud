b8a6ca1a74e3a0876556b73ebb318d7a2bd5b599
yusufbahtiarr <yusufbdev@gmail.com>
2025-07-08 16:43:36 +0700

    chore : run project with docker compose

--- utils/redis.go
+++ utils/redis.go
@@ -13,9 +13,9 @@ var RedisClient *redis.Client
 func Redis() error {
 	godotenv.Load()
 
-	redisAddr := os.Getenv("REDIS_ADDR")
-	redisPassword := os.Getenv("REDIS_PASSWORD")
-	redisDB, _ := strconv.Atoi(os.Getenv("REDIS_DB"))
+	redisAddr := os.Getenv("RDADDRESS")
+	redisPassword := os.Getenv("RDPASSWORD")
+	redisDB, _ := strconv.Atoi(os.Getenv("RDDB"))
 
 	RedisClient = redis.NewClient(&redis.Options{
 		Addr:     redisAddr,
